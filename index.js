f39c08f77e97b7e46cfa710f489b5a0bb805ae16
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2022-08-23 05:50:18 -0700

    added todo list

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const images = [
     './assets/5.jpg',
     './assets/6.jpg'
 ]
+
+// TODO: Step 1: Page Load First Image
+// on page load, select img and set src to first path in images[]
+
+// TODO: Step 2: Load Next Image
+// when user clicks → next btn, set src to next path in images[], change textContent of each bubble btn to ○, change textContent of selected bubble btn to ●
+
+// TODO: Step 3: Load Previous Image
+// when the user clicks the ← prev btn, set src to previous path in images[], change textContent of each bubble btn to ○, change textContent of selected bubble btn to ●
+
+// TODO: Step 4: Select Image
+// when the user clicks a bubble btn, set src to the selected path in images[], change textContent of each bubble btn to ○, change textContent of selected bubble btn to ●
